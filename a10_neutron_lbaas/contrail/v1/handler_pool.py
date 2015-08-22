1a39e5348c64724741e321a291a30e3190313464
Hunter Thompson <hthompson@Hunters-Mac-mini.local>
2015-08-22 14:09:54 -0600

    Complete removal of context from pool handler

--- a10_neutron_lbaas/contrail/v1/handler_pool.py
+++ a10_neutron_lbaas/contrail/v1/handler_pool.py
@@ -36,26 +36,26 @@ class PoolHandler(handler_base_v1.HandlerBaseV1):
         with a10.A10WriteStatusContext(self, pool) as c:
             try:
                 self._set(c.client.slb.service_group.create,
-                          c, context, pool)
+                          c, pool)
             except acos_errors.Exists:
                 pass
 
     def update(self, old_pool, pool):
         with a10.A10WriteStatusContext(self, pool) as c:
             self._set(c.client.slb.service_group.update,
-                      c, context, pool)
+                      c, pool)
 
     def delete(self, pool):
         with a10.A10DeleteContext(self, pool) as c:
             for member in pool['members']:
                 # TODO(teamvnc) - replace with call to contrail ops.
-                m = self.neutron.member_get(context, member)
+                m = self.neutron.member_get(member)
                 self.a10_driver.member._delete(c, m)
 
             for hm in pool['health_monitors_status']:
                 # TODO(teamvnc) - replace with call to contrail ops.
-                z = self.neutron.hm_get(context, hm['monitor_id'])
-                self.a10_driver.hm._delete(c, context, z)
+                z = self.neutron.hm_get(hm['monitor_id'])
+                self.a10_driver.hm._delete(c, z)
 
             if 'vip_id' in pool and pool['vip_id'] is not None:
                 # TODO(teamvnc) - replace with call to contrail ops.
