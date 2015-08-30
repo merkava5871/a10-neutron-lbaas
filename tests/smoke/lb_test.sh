34dec1a69fda87241e22297eb1cb0baa733ba7f8
Michael Durrant <mdurrant@a10networks.com>
2015-08-30 03:00:18 +0000

    Updated smoke test script - get private network. Beginning of smoke test scripts to build up LBaaS infrastructure  using CLI commands for a quick "does it smoke when we turn it on"? test

--- tests/smoke/lb_test.sh
+++ tests/smoke/lb_test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # List networks - if we can't do this, see ya
+NETWORK_NAME="private"
 source test-context.sh
-NETWORK=(neutron net-list | awk { print $2 })
-
-
+NETWORK=$(neutron net-list |grep $NETWORK_NAME | awk '{ print $2 }')
+echo Private network id: $NETWORK
