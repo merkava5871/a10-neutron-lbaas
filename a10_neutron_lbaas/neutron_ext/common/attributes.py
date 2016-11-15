2e467379ebf886af3b0e18e5d06d76c3c87a77ad
Michael Durrant <mdurrant@a10networks.com>
2016-11-15 19:02:56 +0000

    Removed errant merge marker post-rebase Added 'convert_to_list' converter

--- a10_neutron_lbaas/neutron_ext/common/attributes.py
+++ a10_neutron_lbaas/neutron_ext/common/attributes.py
@@ -42,5 +42,7 @@ def _find(*args):
 
 convert_to_int = _find(lambda: old_attributes.convert_to_int,
                        lambda: lib_converters.convert_to_int)
+convert_to_list = _find(lambda: old_attributes.convert_to_list,
+                        lambda: lib_converters.convert_to_list)
 ATTR_NOT_SPECIFIED = _find(lambda: old_constants.ATTR_NOT_SPECIFIED,
                            lambda: lib_constants.ATTR_NOT_SPECIFIED)
